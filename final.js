11dac57a6014815f6cdd09be39c3a946cee1604a
serpallop19 <120453691+serpallop19@users.noreply.github.com>
2022-12-13 10:13:26 +0100

    Add files via upload

---
+++ final.js
@@ -0,0 +1,11 @@
+// Obtenemos la fecha actual y la guardamos en la variable date
+const date = new Date();
+
+// Obtenemos la fecha 30 de Enero de 2018, a las 23h 30m 14seg
+const date = new Date("2018/01/30 23:30:14");
+
+// Obtenemos la fecha del juicio final a partir de un timestamp o Tiempo UNIX
+const date = new Date(872817240000);
+
+// Creamos una fecha pasando cada uno de sus componentes numéricos*
+const date = new Date(2018, 0, 30, 23, 30, 14, 0);
